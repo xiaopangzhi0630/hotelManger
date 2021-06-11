f2182ffc14f2f1b3c9d3374f38b8ad45ed425c17
wenzhiyuan <445917591@qq.com>
2021-06-11 09:28:25 +0800

    api层单独处理数据提交#0611

--- src/views/role/Role.vue
+++ src/views/role/Role.vue
@@ -1,6 +1,5 @@
 <template>
- <div class="app-layout">
-
+  <div class="app-layout">
     <el-row :gutter="20">
       <el-col :span="16">
         <div class="grid-content bg-purple">
@@ -9,120 +8,133 @@
       </el-col>
       <el-col :span="8">
         <div class="grid-content bg-purple">
-          <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-
-            <el-button type="primary" slot="append" icon="el-icon-search"></el-button>
+          <el-input
+            placeholder="请输入内容"
+            v-model="input3"
+            class="input-with-select"
+          >
+            <el-button
+              type="primary"
+              slot="append"
+              icon="el-icon-search"
+            ></el-button>
           </el-input>
         </div>
       </el-col>
     </el-row>
     <el-table :data="tableData" border style="margin-top: 20px">
-      <el-table-column prop="date" label="日期" width="150">
-      </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120">
-      </el-table-column>
+      <el-table-column prop="date" label="日期" width="150"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
       <el-table-column prop="province" label="省份" width="120">
       </el-table-column>
-      <el-table-column prop="city" label="市区" width="120">
-      </el-table-column>
+      <el-table-column prop="city" label="市区" width="120"> </el-table-column>
       <el-table-column prop="address" label="地址" width="300">
       </el-table-column>
-      <el-table-column prop="zip" label="邮编">
-      </el-table-column>
+      <el-table-column prop="zip" label="邮编"> </el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
           <el-button type="text" size="small">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-
 </template>
 
 <script>
-  export default {
-    methods: {
-      handleClick(row) {
-        console.log(row);
-      },
-      purple(){
-      }
-    },
+// 导入角色用户api层
+import * as role_api from '../../api/role_api.js'
 
-    data() {
-      return {
-        input3: '',
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1517 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1519 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }
-        ],
-      }
-    }
-  }
+export default {
+  data() {
+    return {
+      input3: "",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+    purple() {},
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-
 </style>>
